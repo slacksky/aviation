ddda49f94a923bafa8458877d97c66e966b0ed24
slacksky <slacksky@gmail.com>
2021-08-03 22:54:18 -0500

    intiital commit of the  work done

---
+++ src/classes/Planes.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Planes {
+    private int id;
+    private java.lang.String model;
+    private int seats;
+    private int bathrooms;
+    private int max_weight;
+
+    public Planes(int id, String model, int seats, int bathrooms, int max_weight){
+        this.id=id;
+        this.model=model;
+        this.seats=seats;
+        this.bathrooms=bathrooms;
+        this.max_weight=max_weight;
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,13 @@
+
+import classes.*;
+
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Planes
+
+    }
+}
